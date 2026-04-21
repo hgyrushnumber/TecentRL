a8a21b1f2fc53d78254a4a21b0e6316354d6d45e
DayDayUp <128701918+hgyrushnumber@users.noreply.github.com>
2026-04-22 02:18:25 +0800

    Tune SAC hyperparams and rewards; expand action space to 16

--- code/agent_diy/workflow/train_workflow.py
+++ code/agent_diy/workflow/train_workflow.py
@@ -14,7 +14,7 @@ import os
 import time
 
 import numpy as np
-from agent_ppo.feature.definition import SampleData, sample_process
+from agent_diy.feature.definition import SampleData, sample_process
 from tools.metrics_utils import get_training_metrics
 from tools.train_env_conf_validate import read_usr_conf
 from common_python.utils.workflow_disaster_recovery import handle_disaster_recovery
@@ -25,9 +25,9 @@ def workflow(envs, agents, logger=None, monitor=None, *args, **kwargs):
     env = envs[0]
     agent = agents[0]
 
-    usr_conf = read_usr_conf("agent_ppo/conf/train_env_conf.toml", logger)
+    usr_conf = read_usr_conf("agent_diy/conf/train_env_conf.toml", logger)
     if usr_conf is None:
-        logger.error("usr_conf is None, please check agent_ppo/conf/train_env_conf.toml")
+        logger.error("usr_conf is None, please check agent_diy/conf/train_env_conf.toml")
         return
 
     episode_runner = EpisodeRunner(
@@ -99,6 +99,7 @@ class EpisodeRunner:
             done = False
             step = 0
             total_reward = 0.0
+            reward_component_sums = {}
 
             self.logger.info(f"Episode {self.episode_cnt} start")
 
@@ -150,6 +151,9 @@ class EpisodeRunner:
 
                 reward = np.array(next_remain_info.get("reward", [0.0]), dtype=np.float32)
                 total_reward += float(reward[0])
+                reward_components = next_remain_info.get("reward_components", {})
+                for k, v in reward_components.items():
+                    reward_component_sums[k] = reward_component_sums.get(k, 0.0) + float(v)
 
                 final_reward = np.zeros(1, dtype=np.float32)
                 if done:
@@ -157,11 +161,14 @@ class EpisodeRunner:
                     total_score = env_info.get("total_score", 0)
 
                     if terminated:
-                        final_reward[0] = -10.0
-                        result_str = "FAIL"
+                        final_reward[0] = -8.0
+                        result_str = "DEAD"
+                    elif truncated:
+                        final_reward[0] = 12.0
+                        result_str = "TIMEOUT_DONE"
                     else:
-                        final_reward[0] = 10.0
-                        result_str = "WIN"
+                        final_reward[0] = -2.0
+                        result_str = "ABNORMAL"
 
                     self.logger.info(
                         f"[GAMEOVER] episode:{self.episode_cnt} steps:{step} "
@@ -190,6 +197,12 @@ class EpisodeRunner:
                             "reward": round(total_reward + float(final_reward[0]), 4),
                             "episode_steps": step,
                             "episode_cnt": self.episode_cnt,
+                            "final_reward": round(float(final_reward[0]), 4),
+                            "comp_survive": round(reward_component_sums.get("survive_reward", 0.0), 4),
+                            "comp_treasure_score": round(reward_component_sums.get("treasure_score_reward", 0.0), 4),
+                            "comp_danger_penalty": round(reward_component_sums.get("danger_penalty", 0.0), 4),
+                            "comp_dist_shaping": round(reward_component_sums.get("dist_shaping", 0.0), 4),
+                            "comp_repeat_penalty": round(reward_component_sums.get("repeat_explore_penalty", 0.0), 4),
                         }
                         self.monitor.put_data({os.getpid(): monitor_data})
                         self.last_report_monitor_time = now
